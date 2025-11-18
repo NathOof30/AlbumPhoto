8f9ec8488106d4cffa827d7108d8e9cc2bad6e27
Nathanaël <nathanael.leang@gmail.com>
2025-11-18 09:01:30 +0100

    messages en plus

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', [Main::class, 'index']); //accueil
 Route::get('/albums', [Main::class, 'lesAlbums']); //pages des diff albums
 Route::get('/album/{id}', [Main::class, 'detailAlbum'])->where("id", "[0-9]+"); //page montrant les diff photos d'un album
 Route::get('/photos', [Main::class, 'lesPhotos']); //bonus afficher toutes les photos
-Route::get('/tags', [Main::class, 'lesTags']);
+Route::get('/tags', [Main::class, 'lesTags']); //afficher les diff tags
 Route::get('/tag/{id}', [Main::class, 'detailTag'])->where("id", "[0-9]+"); //page montrant les diff photos d'un tag
-Route::get('/formulaire', [Main::class, 'ajoutPhoto']);
-Route::post('/traitementFormulaire', [Main::class, 'traitementFormulaire']);
+Route::get('/formulaire', [Main::class, 'ajoutPhoto']); //formulaire d'ajout de photos
+Route::post('/traitementFormulaire', [Main::class, 'traitementFormulaire']); //traitement du formulaire
