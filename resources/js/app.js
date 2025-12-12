04fe193377a628dd542ac2a87489e13d0ba18d89
Nathanaël <nathanael.leang@gmail.com>
2025-12-12 12:58:12 +0100

    alors vite.js mais pas sûr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import.meta.glob([
+  '../images/**',
+  '../fonts/**',
+]);
+//<link rel="stylesheet" href="{{ asset('style.css') }}">
+//<link rel="stylesheet" href="{{ asset('css/site.css') }}">
+//<link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
