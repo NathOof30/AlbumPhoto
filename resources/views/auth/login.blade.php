361484a5a4678e2523bd8ae12dfbf702ca9afe3c
YanderStudio <lariaric6@gmail.com>
2025-12-16 09:49:53 +0100

    The LAST CSS UPDATE IS CLOSE !!!! (this one isn't actually the last one, but close to!)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,34 +2,35 @@
 @section('content')
     <div class="form-container">
         <h1 class="text-center">Connexion</h1>
-        
-        <form action="{{route("login")}}" method="post" id="login-form">
-            @csrf
-            
-            <div class="form-group">
-                <label for="email">Email :</label>
-                <input type="email" id="email" name="email" required placeholder="votre@email.com">
-            </div>
-            
-            <div class="form-group">
-                <label for="password">Mot de passe :</label>
-                <input type="password" id="password" name="password" required placeholder="Votre mot de passe">
-            </div>
-            
-            <div class="form-group">
-                <label>
-                    <input type="checkbox" name="remember">
-                    Se souvenir de moi
-                </label>
-            </div>
+        <div class="users-form">
+            <form action="{{route("login")}}" method="post" id="login-form">
+                @csrf
+                
+                <div class="form-group">
+                    <label for="email">Email :</label>
+                    <input type="email" id="email" name="email" required placeholder="votre@email.com">
+                </div>
+                
+                <div class="form-group">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" id="password" name="password" required placeholder="Votre mot de passe">
+                </div>
+                
+                <div class="form-group">
+                    <label>
+                        <input type="checkbox" name="remember">
+                        Se souvenir de moi
+                    </label>
+                </div>
 
-            <div class="form-actions">
-                <input type="submit" value="Se connecter" class="btn btn-primary">
-            </div>
-            
-            <p class="text-center text-muted">
-                Pas encore de compte ? <a href="{{route("register")}}">Inscrivez-vous</a>
-            </p>
-        </form>
+                <div class="form-actions">
+                    <input type="submit" value="Se connecter" class="btn btn-primary">
+                </div>
+                
+                <p class="text-center text-muted">
+                    Pas encore de compte ? <a href="{{route("register")}}">Inscrivez-vous</a>
+                </p>
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,37 +2,38 @@
 @section('content')
     <div class="form-container">
         <h1 class="text-center">Inscription</h1>
-        
-        <form action="{{route("register")}}" method="post" id="register-form">
-            @csrf
-            
-            <div class="form-group">
-                <label for="name">Nom :</label>
-                <input type="text" id="name" name="name" required placeholder="Votre nom">
-            </div>
-            
-            <div class="form-group">
-                <label for="email">Email :</label>
-                <input type="email" id="email" name="email" required placeholder="votre@email.com">
-            </div>
-            
-            <div class="form-group">
-                <label for="password">Mot de passe :</label>
-                <input type="password" id="password" name="password" required placeholder="Votre mot de passe">
-            </div>
-            
-            <div class="form-group">
-                <label for="password_confirmation">Confirmer le mot de passe :</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" required placeholder="Confirmez votre mot de passe">
-            </div>
+        <div class="users-form">
+            <form action="{{route("register")}}" method="post" id="register-form">
+                @csrf
+                
+                <div class="form-group">
+                    <label for="name">Nom :</label>
+                    <input type="text" id="name" name="name" required placeholder="Votre nom">
+                </div>
+                
+                <div class="form-group">
+                    <label for="email">Email :</label>
+                    <input type="email" id="email" name="email" required placeholder="votre@email.com">
+                </div>
+                
+                <div class="form-group">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" id="password" name="password" required placeholder="Votre mot de passe">
+                </div>
+                
+                <div class="form-group">
+                    <label for="password_confirmation">Confirmer le mot de passe :</label>
+                    <input type="password" id="password_confirmation" name="password_confirmation" required placeholder="Confirmez votre mot de passe">
+                </div>
 
-            <div class="form-actions">
-                <input type="submit" value="S'inscrire" class="btn btn-primary">
-            </div>
-            
-            <p class="text-center text-muted">
-                Déjà un compte ? <a href="{{route("login")}}">Connectez-vous</a>
-            </p>
-        </form>
+                <div class="form-actions">
+                    <input type="submit" value="S'inscrire" class="btn btn-primary">
+                </div>
+                
+                <p class="text-center text-muted">
+                    Déjà un compte ? <a href="{{route("login")}}">Connectez-vous</a>
+                </p>
+            </form>
+        </div>
     </div>
 @endsection
